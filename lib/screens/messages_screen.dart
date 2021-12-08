752431e296e72d6d8444f8314c2eb9d7a64b8710
gromeroortega <gromeroortega@outlook.com>
2021-12-08 12:52:18 -0600

    Se crearon dos screens básicos y se configuró el proyecto como indica la documentación de FireBase para reibir notificaciones

--- lib/screens/messages_screen.dart
+++ lib/screens/messages_screen.dart
@@ -7,7 +7,7 @@ class MessageScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Messages'),
+        title: const Text('Messages'),
       ),
       body: const Center(
         child: Text('Mesaages'),
